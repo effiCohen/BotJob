bf8553cc344199a39f796689e8cd9a8f25660f20
effiCohen <118920139+effiCohen@users.noreply.github.com>
2024-01-28 17:51:47 +0200

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,30 +1,26 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
-
 //Shlomi
 const userSchema = new mongoose.Schema({
-    FullName:String,
-    email:String,
-    password:String,
-    DateOfBirth:Date,
-    role:String,
-})
-
+  FullName: String,
+  email: String,
+  password: String,
+  DateOfBirth: Date,
+  role: String,
+});
 
 const UserModel = mongoose.model("users", userSchema);
 exports.UserModel = UserModel;
 
 exports.validUser = (_bodyData) => {
   let joiSchema = Joi.object({
-
-    FullName:Joi.string().min(2).max(99).required(),
-    email:Joi.string().min(5).max(99).required(),
-    password:Joi.string().min(2).max(99).required(),
-    DateOfBirth:Joi.date().raw().required(),   
-    role:Joi.string(),
-  })
-
+    FullName: Joi.string().min(2).max(99).required(),
+    email: Joi.string().min(5).max(99).required(),
+    password: Joi.string().min(2).max(99).required(),
+    DateOfBirth: Joi.date().required(),
+    role: Joi.string(),
+  });
 
   return joiSchema.validate(_bodyData);
 };
